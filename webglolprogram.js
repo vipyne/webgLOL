5c9f4266411835126d1dc7bdd66204eda605558f
vipyne <vipyne@gmail.com>
2016-11-24 09:49:32 -0500

    wip

--- webglolprogram.js
+++ webglolprogram.js
@@ -1,20 +1,48 @@
-function webglol() {
+var webglolCanvas,
+    gl,
+    webglolProgram;
+
+var verticesFloatArray;
+var mouseLocation;
+this.mouse = [50, 50];
+var triangleAttributePosition;
+var resolutionLocation;
+var shadersPointerToWorldMatrix;
+var time;
+this.startTime = this.startTime ? this.startTime : new Date().getTime() / 10000;
+var timeLocation;
+var numberOfTriangles = 100;
+var vertices = [];
+
+
+
+function init() {
+  webglolInit();
+  createShaders();
+  locateShaderAttributes();
+  createVertices();
+  draw();
+}
+
+function webglolInit() {
   // get canvas element
-  var webglolCanvas = document.getElementById('webglol');
+  webglolCanvas = document.getElementById('webglol');
   // define WebGLRenderingContext
   //// getContext(context, options)
-  var gl = webglolCanvas.getContext('experimental-webgl');
+  gl = webglolCanvas.getContext('experimental-webgl');
+  webglolProgram = gl.createProgram();
+}
 
-  // Specify the color values used when clearing color buffers.
-  //// gl.clearColor(red, green, blue, alpha)
-  gl.clearColor(0, 0, 0, 0.2);
+util = {
+  matrix : {
+    world : [1,0, 0, 0,
+             0, 1, 0, 0,
+             0,   0,   1, 0,
+             0,   0,   0, 1]
+  }
+}
 
-  // clears buffers to preset values specified by clearColor(), clearDepth() and clearStencil().
-  //// gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT || gl.STENCIL_BUFFER_BIT)
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  var webglolProgram = gl.createProgram();
-
+function createShaders() {
   // time to throw some shade
   var vertexShaderScript = document.getElementById('vertex-shader').text;
   var fragmentShaderScript = document.getElementById('fragment-shader').text;
@@ -37,34 +65,51 @@ function webglol() {
 
   gl.linkProgram(webglolProgram);
   gl.useProgram(webglolProgram);
+}
 
-  var triangleAttributePosition = gl.getAttribLocation(webglolProgram, 'pos');
+function locateShaderAttributes() {
+  // set up pointers from the shader on the card to the values in the app
+
+  // app <------ card
+  triangleAttributePosition = gl.getAttribLocation(webglolProgram, 'pos');
 
   // set the resolution
-  var resolutionLocation = gl.getUniformLocation(webglolProgram, 'u_resolution');
+  resolutionLocation = gl.getUniformLocation(webglolProgram, 'u_resolution');
   gl.uniform2f(resolutionLocation, webglolCanvas.width, webglolCanvas.height);
 
+  shadersPointerToWorldMatrix = gl.getUniformLocation(webglolProgram, 'world');
+  // gl.uniform4f(shadersPointerToWorldMatrix, world);
+
   // timing
-  this.startTime = this.startTime ? this.startTime : new Date().getTime() / 10000;
-  var time = new Date().getTime() / 10000 - this.startTime;
-  var timeLocation = gl.getUniformLocation(webglolProgram, 'u_time');
+  time = new Date().getTime() / 10000 - this.startTime;
+  timeLocation = gl.getUniformLocation(webglolProgram, 'u_time');
   gl.uniform1f(timeLocation, time);
 
-  // MOUSE LOCATION
-  this.mouse;
-  window.addEventListener('mousemove', function(event) {
-    this.mouse = [event.clientX, event.clientY];
-    // return [event.clientX, event.clientY];
-  });
+
   console.log('ttttmouse', this.mouse)
-  console.log('mouse', this.mouse[0]/webglolCanvas.width)
-  var mouseLocation = gl.getUniformLocation(webglolProgram, 'u_mouse');
+  // console.log('mouse', this.mouse[0]/webglolCanvas.width)
+  mouseLocation = gl.getUniformLocation(webglolProgram, 'u_mouse');
   gl.uniform2f(mouseLocation, this.mouse[0]/webglolCanvas.width, this.mouse[1],webglolCanvas.height);
+}
 
-  var vertices = [];
-
+function createVertices() {
   // `O`
   var numberOfTriangles = 100;
+  var degreesPerTriangle = (4 * Math.PI) / numberOfTriangles;
+  var centerX = 0.5;
+
+  for(var i = 0; i < numberOfTriangles; i++) {
+      var index = i * 3;
+      var angle = degreesPerTriangle * i;
+      var scale = 2;
+
+      vertices[index] = Math.cos(angle) / scale;               // x
+      vertices[index + 1] = Math.sin(angle) / scale + centerX; // y
+      vertices[index + 2] = 0;                                 // z
+  }
+
+
+  // `O`
   var degreesPerTriangle = (4 * Math.PI) / numberOfTriangles;
   // var centerX = 1.5;
   // this.mouseX = 1.0;
@@ -73,34 +118,27 @@ function webglol() {
   //   this.mouseX = e.clientX / webglolCanvas.width;
   // console.log('v---', this.mouseX)
   // })
-
   for(var i = 0; i < numberOfTriangles; i++) {
-      var index = i * 3;
-      var angle = degreesPerTriangle * i;
-      var scale = 1;
+    var index = i * 3;
+    var angle = degreesPerTriangle * i;
+    var scale = 1;
   // var centerX = (this.mouse[0]/webglolCanvas.width);
   //   if (centerX > 0.5) {
   //     centerX = centerX
   //   }
-
     // this.mouse[0]/webglolCanvas.width) = x/2;
     // this.mouse[0] * 2 = webglolCanvas.width) * x;
     var centerX = (this.mouse[0] * 2 / webglolCanvas.width) - 1.0;
-
-
-  var centerY = (this.mouse[1] * 2 / webglolCanvas.height) - 1.0;
-
-  console.log(' mouse [x,y]', [this.mouse[0], this.mouse[1]])
-  console.log('[x,y]', [centerX, centerY])
-
-      vertices[index] = Math.cos(angle) / scale + centerX;               // x
-      // vertices[index + 1] = Math.sin(angle) / scale;           // y
-      vertices[index + 1] = Math.sin(angle) / scale; // y
-      vertices[index + 2] = 0;
-
-      console.log('x,y,z', vertices[index + 0])                               // z
-      console.log('x,y,z', vertices[index + 1])                               // z
-      console.log('x,y,z', vertices[index + 2])                               // z
+    var centerY = (this.mouse[1] * 2 / webglolCanvas.height) - 1.0;
+    console.log(' mouse [x,y]', [this.mouse[0], this.mouse[1]])
+    console.log('[x,y]', [centerX, centerY])
+    vertices[index] = Math.cos(angle) / scale + centerX;               // x
+    // vertices[index + 1] = Math.sin(angle) / scale;           // y
+    vertices[index + 1] = Math.sin(angle) / scale; // y
+    vertices[index + 2] = 0;
+    console.log('x,y,z', vertices[index + 0])                               // z
+    console.log('x,y,z', vertices[index + 1])                               // z
+    console.log('x,y,z', vertices[index + 2])                               // z
   }
 
   // `L`s
@@ -111,17 +149,43 @@ function webglol() {
                   0.5, 0.0, 0.0,
                   1.5, 0.0, 0.0 ); // second `L`
 
-  var verticesFloatArray = new Float32Array(vertices);
+  verticesFloatArray = new Float32Array(vertices);
+
+    // MOUSE LOCATION
+  window.addEventListener('mousemove', function(event) {
+    this.mouse = [event.clientX, event.clientY];
+    // return [event.clientX, event.clientY];
+  });
+}
+
+function draw() {
+  // Specify the color values used when clearing color buffers.
+  //// gl.clearColor(red, green, blue, alpha)
+  gl.clearColor(0, 0, 0, 0.2);
+
+  // clears buffers to preset values specified by clearColor(), clearDepth() and clearStencil().
+  //// gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT || gl.STENCIL_BUFFER_BIT)
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+
+
+  gl.uniformMatrix4fv(shadersPointerToWorldMatrix, false, new Float32Array(util.matrix.world));
+  gl.enableVertexAttribArray(shadersPointerToWorldMatrix);
 
   gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
   gl.bufferData(gl.ARRAY_BUFFER, verticesFloatArray, gl.DYNAMIC_DRAW);
+
   gl.enableVertexAttribArray(triangleAttributePosition);
   gl.vertexAttribPointer(triangleAttributePosition, 3, gl.FLOAT, false, 0, 0);
 
   // drawArrays(primatitve shape, start index, number of values to be rendered)
   gl.drawArrays(gl.TRIANGLES, numberOfTriangles, 6); // draw the `L`s
   gl.drawArrays(gl.TRIANGLE_FAN, 0, numberOfTriangles - 5); // draw the `O`
+
+  // window.requestAnimationFrame(callback);
+  // requestAnimationFrame(draw);
 }
 
-window.onload = webglol;
+window.onload = init;
 // setInterval(webglol, 500);
